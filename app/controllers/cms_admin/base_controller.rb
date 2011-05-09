3dd92ab9a98915e586f7daf4685ddc894e76e07f
Jorge Calás Lozano <calas@qvitta.net>
2011-05-09 05:33:11 +0200

    Starting controllers internationalization

--- app/controllers/cms_admin/base_controller.rb
+++ app/controllers/cms_admin/base_controller.rb
@@ -1,33 +1,34 @@
 class CmsAdmin::BaseController < ApplicationController
-  
+
   protect_from_forgery
-  
+
   # Authentication module must have #authenticate method
   include ComfortableMexicanSofa.config.authentication.to_s.constantize
-  
+
   before_filter :authenticate,
                 :load_admin_cms_site
-  
+
   layout 'cms_admin'
-  
+
 protected
-  
+
   def load_admin_cms_site
     hostname = request.host.downcase
     @cms_site = Cms::Site.find_by_hostname!(hostname)
-    
+
   rescue ActiveRecord::RecordNotFound
     unless ComfortableMexicanSofa.config.enable_multiple_sites
       if Cms::Site.count == 0
-        @cms_site = Cms::Site.create!(:label => 'Default Site', :hostname => hostname)
+        @cms_site = Cms::Site.create!(:label => I18n.t('cms.sites.default_site', :default => 'Default Site'), :hostname => hostname)
       else
         @cms_site = Cms::Site.first
         @cms_site.update_attribute(:hostname, hostname)
       end
     end
-    
+
     unless @cms_site
-      flash[:error] = 'No Site defined for this hostname. Create it now.'
+      flash[:error] = I18n.t('cms.controllers.errors.no_site_for_this_hostname',
+                             :default => 'No Site defined for this hostname. Create it now.')
       return redirect_to(cms_admin_sites_path)
     end
   end
